6c57bcfd7278c154a1df7fda818f1d4f62f8b9aa
Andrea <andrea@zampierolo.dev>
2020-10-27 10:09:21 +0100

    Init utilities tab

--- src/components/user_utilities.js
+++ src/components/user_utilities.js
@@ -1,5 +1,17 @@
 import React, { useState } from "react";
-import { IonContent, IonIcon, IonItem, IonLabel, IonList, IonListHeader, IonNote, IonToggle } from "@ionic/react";
+import {
+	IonCard,
+	IonCardContent,
+	IonCardHeader,
+	IonContent,
+	IonIcon,
+	IonItem,
+	IonLabel,
+	IonList,
+	IonListHeader,
+	IonNote,
+	IonToggle,
+} from "@ionic/react";
 import { logInOutline } from "ionicons/icons";
 import { Segment } from "semantic-ui-react";
 
@@ -8,28 +20,30 @@ const UserUtilities = () => {
 	const [checked, setChecked] = useState(false);
 	return (
 		<IonContent>
-			<Segment>
-				<IonList>
-					<IonListHeader>Settings</IonListHeader>
-					<IonItem onClick={() => console.log("click")}>
-						<IonIcon icon={logInOutline} slot="start"></IonIcon>
-						Test login
-						<IonNote slot="end">{login}</IonNote>
-					</IonItem>
-					<IonItem>
-						<IonLabel>Reset Password</IonLabel>
-						<IonToggle
-							checked={checked}
-							onIonChange={(e) => {
-								setChecked(e.detail.checked);
-								console.log("check", e.detail.checked);
-							}}
-							color="danger"
-							slot="end"
-						></IonToggle>
-					</IonItem>
-				</IonList>
-			</Segment>
+			<IonCard>
+				<IonCardHeader>Settings</IonCardHeader>
+				<IonCardContent>
+					<IonList>
+						<IonItem onClick={() => console.log("click")}>
+							<IonIcon icon={logInOutline} slot="start"></IonIcon>
+							Test login
+							<IonNote slot="end">{login}</IonNote>
+						</IonItem>
+						<IonItem>
+							<IonLabel>Reset Password</IonLabel>
+							<IonToggle
+								checked={checked}
+								onIonChange={(e) => {
+									setChecked(e.detail.checked);
+									console.log("check", e.detail.checked);
+								}}
+								color="danger"
+								slot="end"
+							></IonToggle>
+						</IonItem>
+					</IonList>
+				</IonCardContent>
+			</IonCard>
 		</IonContent>
 	);
 };
