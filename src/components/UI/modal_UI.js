991271531911482bfd807f8eda8e7685c8d79b46
Andrea <andrea@zampierolo.dev>
2021-01-06 10:25:38 +0100

    Modals

---
+++ src/components/UI/modal_UI.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import { Card, Dimmer, Loader, Modal } from "semantic-ui-react";
+
+import NotificationForm from "../forms/notification";
+
+const ModalNotification = (props) => {
+	const [dimmerState, setDimmer] = useState(false);
+	useEffect(() => {
+		setDimmer(false);
+	}, []);
+	const handleSubmit = (values) => {
+		setDimmer(true);
+		props.submitNotification({ titolo: values.titolo, contenuto: values.commento });
+	};
+
+	return (
+		<Modal 
+			size="small" 
+			dimmer="inverted"
+			open={props.open} 
+			onClose={() => props.modalToggler(false)}>
+			<Dimmer className={dimmerState ? "active" : ""}>
+				<Loader inverted>Sending...</Loader>
+			</Dimmer>
+			<Modal.Content>
+				<Card>
+					<Card.Content>
+						<Card.Header>
+							{props.type.title_content}
+						</Card.Header>
+					</Card.Content>
+					<Card.Content>
+						<NotificationForm onSubmit={handleSubmit} type={props.type} />
+					</Card.Content>
+				</Card>
+			</Modal.Content>
+		</Modal>
+	);
+};
+
+export default ModalNotification;
